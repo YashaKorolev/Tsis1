4c179614c2b1fb6f1a9d05799df03056f79fc470
YashaKorolev <47750082+YashaKorolev@users.noreply.github.com>
2019-12-14 15:50:28 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,36 +4,36 @@
 #include <iomanip>
 using namespace std;
 
-const double goldenRatio = (1 + sqrt(5)) / 2; // "Золотое" число
+const double goldenRatio = (1 + sqrt(5)) / 2; 
 
-// Рассматриваемая нами функция
+
 double f(double x) {
-	//tanh
+	
 	return cos(x) * tanh(x);
 }
 
 double passive_finder(double a, double b, double eps) {
 	int i = 1;
 	double minx = a;
-	int N = 2 * (b - a) / eps;
-	double step = (b - a) / ((double)N + 1);
-	for (double x = a; x < b; x += step) {
+	double x;
+	int N = (2 * (b - a) / eps)-1;    
+	for (int i = 1; i <= N; i++) {
+		x = a + i * (b - a) / (N + 1);
 		if (f(x) < f(minx)) {
 			minx = x;
 		}
 		cout << i << fixed << std::setprecision(5) << ")x = " << x << fixed << setprecision(6) << " f(x) = " << f(x) << endl;
-		i++;
 	}
 	cout << endl;
-	std::cout << "N= " << N + 2<< " e=+-" << eps / 2 << "  x = " << minx << " f(x) = " << f(minx) << endl;
+	std::cout << "N= " << N<< " e=+-" << eps / 2 << "  x = " << minx << " f(x) = " << f(minx) << endl;
 	return minx;
 }
 
 
 double gold_cut(double a1, double b1, double eps) {
 	int N = 0;
-	double a = a1, b = b1; // Концы отрезка
-	double x1, x2; // Точки, делящие текущий отрезок в отношении золотого сечения
+	double a = a1, b = b1; 
+	double x1, x2; 
 	int i = 1;
 	x1 = b - (b - a) / goldenRatio;
 	x2 = a + (b - a) / goldenRatio;
@@ -41,7 +41,7 @@ double gold_cut(double a1, double b1, double eps) {
 	while (2 * fabs(b - a) > eps) {
 
 
-		if (f(x1) >= f(x2)) { // Условие для поиска минимума
+		if (f(x1) >= f(x2)) { 
 			a = x1;
 			x1 = x2;
 			x2 = a + (b - a) / goldenRatio;
@@ -55,7 +55,7 @@ double gold_cut(double a1, double b1, double eps) {
 		N++;
 		cout << i << ")a=" << a << " b =" << b << "  lenght=" << b - a << "     x1=" << x1 << " f(x1)= " << f(x1) << "x2=" << x2 << "      f(x2)= " << f(x2) << endl;
 		i++;
-	} // Выполняем, пока не достигнем заданной точности
+	} 
 	cout << endl;
 	cout << "N= " << N << "  a= " << a << "   b = " << b << "  lenght=" << b - a << "       x1=" << x1 << "   f(x1)= " << f(x1) << "  x2=" << x2 << " f(x2)= " << f(x2) << endl;
 	return (a + b) / 2;
@@ -65,15 +65,12 @@ double gold_cut(double a1, double b1, double eps) {
 
 int main() {
 
-
-
 	cout << "passive_finder " << endl;
-	double xmin = passive_finder(1.5, 4, 0.1);//26
+	double xmin = passive_finder(1.5, 4, 0.1);
 	cout << endl << endl;
 
 	cout << "gold_cut " << endl;
 	xmin = gold_cut(1.5, 4, 0.1);
 
-	system("pause");
 	return 0;
 }
